8a471f0f92600b280814d8c309e398255bfcb8ef
MoussaMze <ouss_moussa@hotmail.fr>
2015-02-26 17:55:20 +0100

    ajout du client test de la base de données

--- gestioncabinet-client/src/miage/gestioncabinet/client/PlanningTestClientDB.java
+++ gestioncabinet-client/src/miage/gestioncabinet/client/PlanningTestClientDB.java
@@ -1,11 +1,19 @@
 package miage.gestioncabinet.client;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
 import com.novarem.jndi.ServiceLocator;
 import com.novarem.jndi.ServiceLocatorException;
 
 import miage.gestioncabinet.api.GestionCabinetException;
 import miage.gestioncabinet.api.Medecin;
+import miage.gestioncabinet.api.Patient;
 import miage.gestioncabinet.api.PlanningRemoteService;
+import miage.gestioncabinetcoredb.ejbmodule.PatientEntity;
 
 public class PlanningTestClientDB {
 
@@ -28,11 +36,27 @@ public class PlanningTestClientDB {
 		System.out.println("On developpe un scenario de test du planning de consultation en mode persistance");
 		
 		try {
-			System.out.println("Creation");
-
+			System.out.println("Creation Medecin");
 			Medecin m = app.ejb.getMedecin();
-			System.out.println("affichage");
-			//System.out.println(m.getPrenom());
+			System.out.println("On recupere la liste de medecin");
+			List<Medecin> lm = app.ejb.rechercherMedecins();
+			System.out.println(lm.size());
+			System.out.println("On recupere la liste de patients");
+				Calendar birth = Calendar.getInstance();
+
+				birth.set(Calendar.YEAR, 1991);
+				birth.set(Calendar.MONTH, 0);
+				birth.set(Calendar.DAY_OF_MONTH, 16);
+				birth.set(Calendar.HOUR_OF_DAY, 0);
+				birth.set(Calendar.MINUTE, 0);
+				birth.set(Calendar.SECOND, 0);
+				birth.set(Calendar.MILLISECOND, 0);
+				
+				List<Patient> patients = app.ejb.rechercherPatients("MOUSSA MZE", "Oussama", birth);
+				System.out.println("Taille -> " + patients.size());
+				System.out.println("Date -> " + patients.get(0).getDateNaissance().getTime());
+
+
 		} catch (Exception e) {
 			System.out.println(e.fillInStackTrace());
 			e.printStackTrace();
